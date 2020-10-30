bba15c44d63dc6c268e6bcde6073f062cdca663f
nvaldez <nolivaldeziii@gmail.com>
2020-10-30 19:32:32 +0800

    remove argparse dependency

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ setup(
   author = 'Noli Valdez III',                   # Type in your name
   author_email = 'nolivaldeziii@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/nvaldeziii/AutoHtml',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/nvaldeziii/AutoHtml/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/nvaldeziii/AutoHtml/archive/1.0.tar.gz',    # I explain this later on
   keywords = ['html', 'oop'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
